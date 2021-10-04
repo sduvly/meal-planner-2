a4f38d7b3996ed378783582953da26b221d89896
sduvly <sduvly@gmail.com>
2021-10-04 17:55:23 -0400

    push1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import RecipeCard from "./RecipeCard"
+import{useState, useEffect} from "react"
 
 function App() {
+  const [isrecipe, setIsrecipe] = useState([])
+  useEffect(() => {
+    fetch("www.themealdb.com/api/json/v1/1/search.php?f=a")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <RecipeCard />
     </div>
   );
 }
---
+++ src/RecipeCard.js
@@ -0,0 +1,5 @@
+
+function RecipeCard(){
+ 
+}
+export default RecipeCard;
